bf53ab92da8f18b809abb0df6028811f9a463a79
yanyongjun <yanyojun@foxmail.com>
2019-05-30 12:06:56 +0800

    Initial commit

---
+++ src/main/LeetCode434.java
@@ -0,0 +1,26 @@
+package main;
+
+public class LeetCode434 {
+    public static void main(String... args) {
+        System.out.println(new Solution().countSegments("Hello, my name is John"));
+    }
+}
+
+class Solution {
+    public int countSegments(String s) {
+        if (s == null) {
+            return 0;
+        }
+        s = s.trim();
+        if(s.length() == 0){
+            return 0;
+        }
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == ' ' && s.charAt(i-1) != ' ') {
+                count++;
+            }
+        }
+        return count + 1;
+    }
+}
